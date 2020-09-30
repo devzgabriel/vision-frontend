4c37d24aec4a26c8b7107f56a024b3757cbb5003
Gabriel Silva <gabriel.etefmc@gmail.com>
2020-09-30 12:03:53 -0300

    back api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const api = axios.create({
   // baseURL: 'https://localhost:3333/'
-  baseURL: 'https://vision-b.herokuapp.com/'
+  baseURL: 'https://server-vision.herokuapp.com/'
 })
 
 export default api
